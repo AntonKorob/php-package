0a23018561e0aacff534e8251e947864e6101746
AntonKorob <gorizont102.k@mail.ru>
2025-03-21 23:07:51 +0700

    add UserTest.php

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -2,9 +2,13 @@
 
 namespace Php\Package\Tests;
 
+require("src/Utils.php");
+require("vendor/autoload.php");
+
 use PHPUnit\Framework\TestCase;
 use Php\Package\User;
 
+use function Hexlet\Phpunit\Utils\reverseString;
 class UserTest extends TestCase
 {
     public function testGetName(): void
@@ -15,5 +19,11 @@ class UserTest extends TestCase
 
         $this->assertEquals($name, $user->getName());
         $this->assertEquals(collect($children), $user->getChildren());
+    }
+
+    public function testReverse()
+    {
+        $this->assertEquals('', reverseString(''));
+        $this->assertEquals('olleH', reverseString('Hello'));
     }
 }
